ca4381b5d6de8262ad222ff1db68152ba0628ca5
alesha <shegetnpanda@gmail.com>
2023-01-23 20:33:34 +0600

    small change 2

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'shegetnpanda@gmail.com';                 // Наш логин
-$mail->Password = 'mhevqdsbkkxnfofn';                           // Наш пароль от ящика
+$mail->Password = 'zrnrdzwqktcuusaw';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';       	                     // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
